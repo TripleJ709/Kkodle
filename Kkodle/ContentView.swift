a65a2823ece082d635ad7d8f1ab33e6cf1bfbc96
TripleJ709 <ijn9907@gmail.com>
2026-07-23 00:39:49 +0900

    프로젝트 초기 세팅

---
+++ Kkodle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kkodle
+//
+//  Created by 장주진 on 7/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kkodle/KkodleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KkodleApp.swift
+//  Kkodle
+//
+//  Created by 장주진 on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct KkodleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
